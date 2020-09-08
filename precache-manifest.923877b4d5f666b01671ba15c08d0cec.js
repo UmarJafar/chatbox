12b2814c82e6cd34599e0b081e0c8f7f9b90da09
umarjafar <hm.umar001@gmail.com>
2020-09-08 15:21:09 +0500

    Updates

---
+++ precache-manifest.923877b4d5f666b01671ba15c08d0cec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bbff8f84756fac48ca62eb7e654ce46",
+    "url": "/chatbox/index.html"
+  },
+  {
+    "revision": "58131f7f64539d4fb9e3",
+    "url": "/chatbox/static/css/main.a4f84af9.chunk.css"
+  },
+  {
+    "revision": "ab6667cbfc8140e900bf",
+    "url": "/chatbox/static/js/2.8560624e.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/chatbox/static/js/2.8560624e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58131f7f64539d4fb9e3",
+    "url": "/chatbox/static/js/main.4a22089d.chunk.js"
+  },
+  {
+    "revision": "4101ac67425087328503",
+    "url": "/chatbox/static/js/runtime-main.c0d9ab87.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e739fb07444a07e51e8764e7d5f68885.js"
+  "/chatbox/precache-manifest.923877b4d5f666b01671ba15c08d0cec.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chatbox/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
