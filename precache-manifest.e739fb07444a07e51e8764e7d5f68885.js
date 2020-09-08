ea510ae4eb8d09b9c68f2e355e275da75a9ae832
umarjafar <hm.umar001@gmail.com>
2020-09-08 12:56:31 +0500

    Updates

---
+++ precache-manifest.e739fb07444a07e51e8764e7d5f68885.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5629c3b2779da8169a9987bf55fedc48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58131f7f64539d4fb9e3",
+    "url": "/static/css/main.a4f84af9.chunk.css"
+  },
+  {
+    "revision": "ab6667cbfc8140e900bf",
+    "url": "/static/js/2.8560624e.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.8560624e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58131f7f64539d4fb9e3",
+    "url": "/static/js/main.4a22089d.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
